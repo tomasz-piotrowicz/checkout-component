6eb73c99c027983759a2ddd27899ea303a500dd4
tomasz-piotrowicz <pantomash@gmail.com>
2018-02-14 20:51:05 +0100

    first commit

---
+++ src/main/java/com/checkout/CheckoutComponentApplication.java
@@ -0,0 +1,12 @@
+package com.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutComponentApplication.class, args);
+	}
+}
